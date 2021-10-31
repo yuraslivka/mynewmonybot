02b6dd368f0d8f5448e282ec96ac41a74c3a98a8
yuraslivka <slivka.y@gmail.com>
2021-10-31 17:51:52 +0100

    311021

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import config
 import logging
 import asyncio
-import aioschedule
+import requests
 from datetime import datetime
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from keybords import kb_client
-import os
+from bs4 import BeautifulSoup as BS
 
 
 # задаем уровень логов
@@ -35,7 +35,14 @@ async def send_welcome(message: types.Message):
 @dp.message_handler(commands=['h'])
 async def send_welcome(message: types.Message):
 
-    await bot.send_message(message.from_user.id, 'magic &*&')
+    r = requests.get("https://finance.i.ua/bank/115/")
+    html = BS(r.content, 'html.parser')
+
+    for el in html.select('.data_container > table'):
+        title = el.select('span')
+        print(title[4].text)
+
+    await bot.send_message(message.from_user.id, title[4].text)
 
 
 @dp.message_handler()
@@ -51,21 +58,6 @@ async def echo(message: types.Message):
             await message.answer(6*b)
 
 
-async def noon_print():
-    print("It's noon!")
-
-
-async def scheduler():
-    aioschedule.every().day.at("13:50").do(noon_print)
-    while True:
-        await aioschedule.run_pending()
-        await asyncio.sleep(1)
-
-
-async def on_startup(_):
-    asyncio.create_task(scheduler())
-
-
 # запускаем лонг поллинг
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
+    executor.start_polling(dp, skip_updates=True)
---
+++ test.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+r = requests.get("https://finance.i.ua/bank/115/")
+html = BS(r.content, 'html.parser')
+
+for el in html.select('.data_container > table'):
+    title = el.select('span')
+    print(title[4].text)
